87238f9eda45b97fae19e2bf85748a7e71b76eb4
99-Knots <155378311+99-Knots@users.noreply.github.com>
2026-03-26 04:16:42 +0100

    first draft of graph representation

--- src/CrochetGraph.ts
+++ src/CrochetGraph.ts
@@ -1,9 +1,9 @@
-type StitchType = "ch" | "ss" | "sc" | "hdc" | "dc" | "tr";
+type StitchType = "ss" | "sc" | "hdc" | "dc" | "tr" | "ch" | "hole" | "mc";
 
 
 class Modifier {
-    type: "p" | "l" | "" = "";
-    position: "f" | "b" = "f";
+    private type: "p" | "l" | "" = "";
+    private position: "f" | "b" = "f";
 
     constructor(type?: "p" | "l" | "", position?: "f" | "b") {
         if(type)
@@ -24,53 +24,106 @@ class Modifier {
     }
 }
 
-const blMod = new Modifier("l", "b");
-const flMod = new Modifier("l");
-const bpMod = new Modifier("p", "b");
-const fpMod = new Modifier("p");
-const emptyMod = new Modifier()
+export const Modifiers = {
+    BL: new Modifier("l", "b"),
+    FL: new Modifier("l"),
+    BP: new Modifier("p", "b"),
+    FP: new Modifier("p"),
+    NO: new Modifier
+};
 
+export const StitchTypes = {
+    CH: "ch",
+    SLST: "ss",
+    SC: "sc",
+    HDC: "hdc",
+    DC: "dc",
+    TR: "tr"
+}
 
-class Node {
-    previous: Node | null = null;
-    next: Node | null = null;
-    children: Node[] | null = null;
-    parents: Node[] | null = null;
-    type: StitchType = "sc";
-    modifier: Modifier;
-    name: string;
+type EdgeType = "prev" | "slst" | "insert" | "surround";
 
-    constructor(name: string) {
-        this.name = name;
-        this.modifier = emptyMod;
+class Edge {
+    to: Vertex;
+    from: Vertex;
+    type: EdgeType;
+    mod: Modifier;
+
+    constructor(from: Vertex, to: Vertex, type: EdgeType, mod: Modifier) {
+        this.to = to;
+        this.from = from;
+        this.type = type;
+        this.mod = mod;
+    }
+}
+
+class Vertex {
+    id: string;
+    layer: number;
+    type: StitchType;
+
+    constructor(id: string, type: StitchType, layer: number) {
+        this.id = id;
+        this.layer = layer;
+        this.type = type;
+    }
+}
+
+export class Pattern {
+    edges: Edge[] = [];
+    vertices: Map<string, Vertex> = new Map();
+    currentRow: number = 0;
+
+    addVertex(v: Vertex) {
+        this.vertices.set(v.id, v);
     }
 
-    insertBefore(other: Node) {
-        const temp = other.previous;
-
-        // connect to other
-        other.previous = this;
-        this.next = other;
-
-        // connect to previous previous
-        if(temp)
-            temp.next = this;
-        this.previous = temp;
+    addEdge(fromID: string, toID: string, type: EdgeType, mod: Modifier=Modifiers.NO) {
+        const source = this.vertices.get(fromID);
+        const target = this.vertices.get(toID);
+        if(source && target)
+            this.edges.push(new Edge(source, target, type, mod))
     }
 
-    insertAfter(other: Node) {
-        const temp = other.next;
-
-        other.next = this;
-        this.previous = other;
-
-        if(temp)
-            temp.previous = this;
-        this.next = temp;
+    startRing(n: number) {
+        if(n<2) {
+            return null;
+        }
+        const firstCh = new Vertex("0", "ch", 0);
+        this.addVertex(firstCh);
+        let lastID = firstCh.id;
+        const hole = new Vertex("h0", "hole", 0);
+        this.addVertex(hole)
+        for(let i=1;i<n;i++) {
+            const ch = new Vertex(String(i), "ch", 0);
+            this.addVertex(ch);
+            this.addEdge(lastID, ch.id, "prev")
+            this.addEdge(ch.id, hole.id, "surround");
+            lastID = ch.id;
+        }
+        this.addEdge(lastID, firstCh.id, "slst");
+        return hole;
     }
 
-    symbol() {
-        let str = this.modifier.applyToStitch(this.type);
-        return str;
+    getRowIDs(row: number) {
+        const IDs: Vertex[] = [];
+        // TODO
+    }
+
+    addRow(previousRowIDs: string[]) {
+        this.currentRow += 1;
+        let previousID: string | null = null;
+        for(const parentID of previousRowIDs) {
+            const stitchNr = Math.random() < 0.5 ? 1 : 2;
+            for(let i=0;i<stitchNr;i++) {
+                const stitch = new Vertex(parentID+String(this.currentRow)+String(), "sc", this.currentRow);
+                this.addVertex(stitch);
+                console.log(stitchNr);
+                if(previousID)
+                    this.addEdge(stitch.id, previousID, "prev");
+                this.addEdge(stitch.id, parentID, "insert");
+                previousID = stitch.id;
+            }
+        }
     }
 }
--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,14 @@
 import './style.css'
 import * as LSys from './LSystem.ts'
+import * as CG from './CrochetGraph.ts'
 
 const pattern = document.getElementById("pattern")!;
 let circle = new LSys.LSystem();
 circle.generate(6);
 pattern.innerText = circle.formatToGrammar();
+
+const pat = new CG.Pattern();
+let h = pat.startRing(5);
+if(h)
+    pat.addRow([h.id]);
+console.log(pat);
